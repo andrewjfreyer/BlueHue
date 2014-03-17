133df47d3fce2e0f4d3a17c615b28cbd09502ca4
Andrew J Freyer <andrew.freyer@gmail.com>
2014-03-17 17:32:20 -0600

    Updates

--- blue_hue.sh
+++ blue_hue.sh
@@ -96,7 +96,7 @@ while ($1); do
 			if [ "$laststatus" != 1 ]; then  
 				#bluetooth device arrived
 				notify "All lights have been turned on."
-				type hue_allon_custom
+				hue_allon_custom
 				laststatus=1
 			else
 				#bluetooth device remains present.
